63f2851580485131d484c799b2184c6db6e61d02
johanmezapaez <johanjm@uninorte.edu.co>
2022-11-16 23:26:51 -0500

    Se sube el proyecto a github

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+<Routes>
+<Route exact path="/" element={<App/>} />
+<Route path="/index" element={<App2/>} />
+</Routes>
+</Router>
+
   </React.StrictMode>
 );
 
