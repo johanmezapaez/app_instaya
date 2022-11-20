d38dd933e988206269fbb128be5ad1e08659663b
johanmezapaez <johanjm@uninorte.edu.co>
2022-11-20 13:32:12 -0500

    Se actualiza conponents Login y Register

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link} from "react-router-dom";
 export const Login = () => {
   return (
     <div>
@@ -18,17 +18,17 @@ export const Login = () => {
                     <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Bienvenido!</h1>
+                                <h1 class="h4 text-gray-900 mb-4">Iniciar sesión</h1>
                             </div>
                             <form class="user">
                                 <div class="form-group">
                                     <input type="email" class="form-control form-control-user"
                                         id="exampleInputEmail" aria-describedby="emailHelp"
-                                        placeholder="Enter Email Address..."/>
+                                        placeholder="Correo electrónico"/>
                                 </div>
                                 <div class="form-group">
                                     <input type="password" class="form-control form-control-user"
-                                        id="exampleInputPassword" placeholder="Password"/>
+                                        id="exampleInputPassword" placeholder="Contraseña"/>
                                 </div>
                                 <div class="form-group">
                                     <div class="custom-control custom-checkbox small">
@@ -37,23 +37,23 @@ export const Login = () => {
                                             Me</label>
                                     </div>
                                 </div>
-                                <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                    Login
-                                </a>
+                                <Link to="/index" class="btn btn-primary btn-user btn-block">
+                                    Iniciar sesión
+                                </Link>
                                 <hr/>
                                 <a href="index.html" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Login with Google
+                                    <i class="fab fa-google fa-fw"></i> Iniciar con Google
                                 </a>
                                 <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                    <i class="fab fa-facebook-f fa-fw"></i> Iniciar con Facebook
                                 </a>
                             </form>
                             <hr/>
                             <div class="text-center">
-                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                <a class="small" href="forgot-password.html">¿Olvidaste tu contraseña?</a>
                             </div>
                             <div class="text-center">
-                                <a class="small" href="register.html">Create an Account!</a>
+                                <Link to="/Register" class="small">¿Aún no tienes cuenta?</Link>
                             </div>
                         </div>
                     </div>
