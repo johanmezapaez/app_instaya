d375116f789b7b300fa73e4d237e0b56ad6702f4
johanmezapaez <johanjm@uninorte.edu.co>
2022-11-21 01:56:17 -0500

    Se carga el componente Forgotpassword

--- src/Components/Forgotpassword.js
+++ src/Components/Forgotpassword.js
@@ -17,26 +17,25 @@ export const Forgotpassword = () => {
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-2">Forgot Your Password?</h1>
-                                        <p class="mb-4">We get it, stuff happens. Just enter your email address below
-                                            and we'll send you a link to reset your password!</p>
+                                        <h1 class="h4 text-gray-900 mb-2">Recuperar contraseña</h1>
+                                        <p class="mb-4">Introduce la dirección de correo electrónico asociada a tu cuenta y te enviaremos un correo para restablecer tu contraseña.</p>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address..."/>
+                                                placeholder="Correo electrónico"/>
                                         </div>
-                                        <a href="login.html" class="btn btn-primary btn-user btn-block">
-                                            Reset Password
-                                        </a>
+                                        <Link to="/Login" class="btn btn-primary btn-user btn-block">
+                                            Enviar
+                                        </Link>
                                     </form>
                                     <hr/>
                                     <div class="text-center">
-                                        <Link to="/Register" class="small">Create an Account!</Link>
+                                        <Link to="/Register" class="small">¿Aún no tienes cuenta?</Link>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="login.html">Already have an account? Login!</a>
+                                        <Link to="/Login" class="small">¿Ya tienes cuenta? Entra aquí!</Link>
                                     </div>
                                 </div>
                             </div>
